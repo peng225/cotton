7d963fee6c97b2c72a8a52995771330a4d66f0d1
Shinya Hayashi <shinyaUT0925@gmail.com>
2022-11-26 10:51:11 +0900

    make the memory store thread-safe.
    Signed-off-by: Shinya Hayashi <shinyaUT0925@gmail.com>

--- storage/mem_store.go
+++ storage/mem_store.go
@@ -1,8 +1,12 @@
 package storage
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 type MemoryStore struct {
+	mu   sync.Mutex
 	data map[string][]byte
 }
 
@@ -13,10 +17,14 @@ func NewMemoryStore() *MemoryStore {
 }
 
 func (ms *MemoryStore) Add(key string, value []byte) {
+	ms.mu.Lock()
+	defer ms.mu.Unlock()
 	ms.data[key] = value
 }
 
 func (ms *MemoryStore) Get(key string) ([]byte, error) {
+	ms.mu.Lock()
+	defer ms.mu.Unlock()
 	value, ok := ms.data[key]
 	if !ok {
 		return nil, fmt.Errorf("key not found: key = %s", key)
@@ -25,5 +33,7 @@ func (ms *MemoryStore) Get(key string) ([]byte, error) {
 }
 
 func (ms *MemoryStore) Delete(key string) {
+	ms.mu.Lock()
+	defer ms.mu.Unlock()
 	delete(ms.data, key)
 }
